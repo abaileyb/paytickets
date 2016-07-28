4bcf4139079660411f19e4662aa556632571d1d9
Bailey Berro <bailey@baileyberro.com>
2016-07-28 12:18:19 -0700

    working. needs to be pushed to python anywhere during schoolyear

--- email_handler.py
+++ email_handler.py
@@ -4,9 +4,9 @@ import time
 
 def send_noTicketsFound_message():
     return requests.post(
-        "https://api.mailgun.net/v3/sandbox3834bdc776b7485682e2d83b3c65bc0e.mailgun.org/messages",
+        "https://api.mailgun.net/v3/mg.baileyberro.com/messages",
         auth=("api", "key-e2fbc66c1bedbcd47649b34cbac6df55"),
-        data={"from": "TicketAutoPay <postmaster@sandbox3834bdc776b7485682e2d83b3c65bc0e.mailgun.org>",
+        data={"from": "TicketAutoPay <postmaster@mg.baileyberro.com>",
               "to": "Bailey <bailey@baileyberro.com>",
               "subject": "Ticket Payment For "  + time.strftime("%x"),
               "text": "Congrats! There were no tickets found!"})
@@ -14,9 +14,9 @@ def send_noTicketsFound_message():
 
 def send_TicketsFound_message(numTickets, totalCost):
     return requests.post(
-        "https://api.mailgun.net/v3/sandbox3834bdc776b7485682e2d83b3c65bc0e.mailgun.org/messages",
+        "https://api.mailgun.net/v3/mg.baileyberro.com/messages",
         auth=("api", "key-e2fbc66c1bedbcd47649b34cbac6df55"),
-        data={"from": "TicketAutoPay <postmaster@sandbox3834bdc776b7485682e2d83b3c65bc0e.mailgun.org>",
+        data={"from": "TicketAutoPay <postmaster@mg.baileyberro.com>",
               "to": "Bailey <bailey@baileyberro.com>",
               "subject": "Ticket Payment For "  + time.strftime("%x"),
-              "text": "Paid " + numTickets + " tickets for a cost of $" + totalCost })
+              "text": "Paid " + numTickets + " tickets for a cost of " + totalCost })
--- pay_tickets.py
+++ pay_tickets.py
@@ -7,6 +7,10 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException   
 
 from email_handler import *
+from personal_info import paymentInfo
+from personal_info import carInfo
+
+
 ## Helpers
 
 def check_exists_by_xpath(xpath, driver):
@@ -23,10 +27,10 @@ driver.get("https://www.parkingticketpayment.com/ticketpay/index.php?imsvil=5A")
 
 # First Page #
 select_veh_state = Select(driver.find_element_by_id("veh_state"))
-select_veh_state.select_by_value("NY")
+select_veh_state.select_by_value(carInfo["state"])
 
 enter_veh_plate = driver.find_element_by_id("veh_plate")
-enter_veh_plate.send_keys('GTE2801')
+enter_veh_plate.send_keys(carInfo["plate"])
 
 button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "continue")))
 button.click()
@@ -44,6 +48,35 @@ else:
 
 
 # Third Page #
+number_of_tickets = driver.find_element_by_xpath("/html/body/div[2]/div/form/div[3]/h4")
+number_of_tickets = number_of_tickets.text.rsplit(None, 1)[-1]
+
+total_amount = driver.find_element_by_xpath("/html/body/div[2]/div/form/div[5]/table/tbody/tr[3]/td[2]/span")
+total_amount = total_amount.text
+
+button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "pay")))
+button.click()
 
 
+# Fourth Page #
+text_fields = ["name", "streetAddress", "city", "zipCode", "accountNumber", "cvv2", "email"]
+for x in text_fields:
+	enter = driver.find_element_by_name(x)
+	enter.send_keys(paymentInfo[x])
+
+
+select_fields = ["state", "month", "year"]
+for x in select_fields:
+	enter = Select(driver.find_element_by_name(x))
+	enter.select_by_value(paymentInfo[x])
+
+button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "continue")))
+button.click()
+
+
+# Fifth Page #
+send_TicketsFound_message(number_of_tickets, total_amount)
+
+button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "continue")))
+button.click()
 
